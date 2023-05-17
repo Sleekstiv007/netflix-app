dff0011dae05eaf3238f5a3760fc6517fc551219
Sleekstiv007 <smiene13@gmail.com>
2023-05-17 17:08:34 +0100

    completed homepage

---
+++ src/components/Row.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import Movie from "./Movie";
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4 ">{title}</h2>
+      <div className="relative flex items-center "></div>
+      <div
+        id={"slider"}
+        className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+      >
+        {movies.map((item, id) => (
+          <Movie key={id} item={item} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Row;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import request from "../Request";
 
 const Home = () => {
   return (
-    <div>
-      <Main/>
-    </div>
-  )
-}
+    <>
+      <Main />
+      <Row title="Up Coming" fetchURL={request.requestUpcoming} />
+      <Row title="Popular" fetchURL={request.requestPopular} />
+      <Row title="Trending" fetchURL={request.requestTrending} />
+      <Row title="Top Rated" fetchURL={request.requestTopRated} />
+      <Row title="Horror" fetchURL={request.requestHorror} />
+    </>
+  );
+};
 
-export default Home
+export default Home;
